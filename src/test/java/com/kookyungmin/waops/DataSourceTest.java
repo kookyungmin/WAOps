a7e9e15ca6b8bf248d4eec4819249fcdc1f97a0e
gguri4549 <rudals4549@naver.com>
2018-09-21 14:57:30 +0900

    update

---
+++ src/test/java/com/kookyungmin/waops/DataSourceTest.java
@@ -0,0 +1,32 @@
+package com.kookyungmin.waops;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.mysql.jdbc.Connection;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
+
+public class DataSourceTest {
+	private static Logger logger = LoggerFactory.getLogger(DataSourceTest.class);
+	
+	@Inject
+	private DataSource ds;
+	
+	@Test
+	public void testConnection() throws Exception{
+		try(Connection con = (Connection) ds.getConnection()){
+			logger.debug("DataSourceTest>>>{}",con);
+		}catch(Exception e) {
+			logger.debug(e.getMessage());
+		}
+	}
+}
---
+++ src/test/java/com/kookyungmin/waops/SqlSessionFactoryTest.java
@@ -0,0 +1,35 @@
+package com.kookyungmin.waops;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/**/root-context.xml")
+public class SqlSessionFactoryTest {
+	private static Logger logger = LoggerFactory.getLogger(SqlSessionFactoryTest.class);
+	
+	@Inject
+	private SqlSessionFactory sqlFactory;
+	
+	@Test
+	public void testFactory() {
+		logger.debug("testFactory>>>>{}",sqlFactory);
+	}
+	@Test
+	public void testSession() {
+		try(SqlSession session = sqlFactory.openSession()){
+			logger.debug("testSession>>>{}", session);
+		}catch(Exception e) {
+			logger.debug(e.getMessage());
+		}
+	}
+	
+}
