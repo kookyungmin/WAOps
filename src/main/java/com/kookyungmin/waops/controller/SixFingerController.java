b5004e4b1b2f19645fe7b0812769c2fd8435d06a
gguri4549 <rudals4549@naver.com>
2018-10-09 22:03:57 +0900

    update

--- src/main/java/com/kookyungmin/waops/controller/SixFingerController.java
+++ src/main/java/com/kookyungmin/waops/controller/SixFingerController.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,15 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/sixfinger")
 public class SixFingerController {
 	private static Logger logger = LoggerFactory.getLogger(SixFingerController.class);
+	private static String sendMessage = "on";
 	
-	@RequestMapping(value = "/light", method = RequestMethod.POST)
-	public ResponseEntity<String> lightSwitch(@RequestBody String state){
+	@RequestMapping(value = "/light/{state}", method = RequestMethod.GET)
+	public ResponseEntity<String> lightSwitch(@PathVariable("state") String state){
 		logger.debug("SixFingerController.lightSwitch()>>>> state={}", state);
 		try {
+			sendMessage = state;
 			return new ResponseEntity<>(state, HttpStatus.OK);
 		}catch(Exception e) {
 			return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
 		}
-		
+	}
+	@RequestMapping(value = "/sendArduino", method = RequestMethod.GET)
+	public ResponseEntity<String> sendArduino(){
+		try {
+			return new ResponseEntity<>(sendMessage, HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
 	}
 }
